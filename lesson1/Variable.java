f7b81ddd9a67e51ba67c8be7f94f09b4f0404b78
piv1991 <piv25041991@gmail.com>
2019-03-26 22:16:53 +0300

    Объявлены все примитивные переменные с последующим выводом в консоль

---
+++ lesson1/Variable.java
@@ -0,0 +1,13 @@
+public class Variable { 
+    public static void main(String[] args) {
+        byte TypeSystem = 64;
+        short Hdd = 682;
+        int GeForce = 650;
+        long Windows = 10;
+        float Ram = 8.00f;
+        double Cpu = 2.40;
+        char IntelCoreI = 7;
+        boolean Laptop = true;
+        System.out.println (TypeSystem + " " + Hdd + " " + GeForce + " " + Windows + " " + Ram + " " + Cpu + " " + IntelCoreI );
+    }
+}
