02716981d65afdd904498f192bd4343f5d50c6c2
piv1991 <piv25041991@gmail.com>
2019-03-28 22:40:28 +0300

    не понятно

---
+++ lesson1/ConditionalStatement.java
@@ -0,0 +1,29 @@
+public class ConditionalStatement { 
+    public static void main(String[] args) {
+int yourAge = 27
+double yourHeight = 1.83
+String yourName = "Илья"
+boolean
+
+if (yourAge > 20) {
+    System.out.println ("пора становиться серьёзнее");
+}
+if (youAreMale) {
+    System.out.println ("готовься к трудностям");
+}
+if (youAreFemale) {
+    System.out.println ("третий размер открывает тысячи дорог");
+}
+if (yourHeight < 1.80) 
+    System.out.println ("волосатый, мускулистый");
+} else {
+    System.out.println ("Да, у тебя же папа пианист...");
+}
+if (yourNameStartsWithTheLetterМ == ‘М’) {
+    System.out.println ("оченькрасивое имя");
+} else if (yourNameStartsWithTheLetterИ == ‘И’) {
+    System.out.println ("это лучшее имя на Земле");
+} else {
+    System.out.println ("приятно познакомиться");
+          }
+}
