43ba9fa4b3e6af9c713f8d8cbebae55f415d8be1
piv1991 <piv25041991@gmail.com>
2019-03-31 09:33:51 +0300

    tab

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -9,25 +9,24 @@ int yourAge = 27;
 if (yourAge > 20) {
     System.out.println ("пора становиться серьёзнее");
 }
-
 String gender = man;
 boolean isGender = (gender == man);
-if (isGender == true) {
+  if (isGender == true) {
     System.out.println ("готовься к трудностям");
 }
-if (isGender == false) {
+  if (isGender == false) {
     System.out.println ("третий размер открывает тысячи дорог");
 }
 
 double yourHeight = 1.83;
-if (yourHeight < 1.80) {
+  if (yourHeight < 1.80) {
     System.out.println ("волосатый, мускулистый");
 } else {
     System.out.println ("Да, у тебя же папа пианист...");
 }
 
 if (yourNameStartsWithTheLetterМ == ‘М’) {
-    System.out.println ("оченькрасивое имя");
+    System.out.println ("очень красивое имя");
 } else if (yourNameStartsWithTheLetterИ == ‘И’) {
     System.out.println ("это лучшее имя на Земле");
 } else {
